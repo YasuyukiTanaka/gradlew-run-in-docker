3dbc302ffd18aef3e531a215e7137043370f66f7
YasuyukiTanaka <yasuyuki.tanaka@sony.com>
2020-05-21 09:33:57 +0900

    2020/0t5/21 9:32

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,12 +1,17 @@
 #!/bin/bash
 
 
-task=$1
-workingDirectory=$2
+TASK=$1
+WORK_DIREDTORY=$2
 
-cd ${GITHUB_WORKSPACE}/${workingDirectory}
+cd ${GITHUB_WORKSPACE}/${WORK_DIREDTORY}
 
 
-./gradlew $task
+./gradlew ${TASK}
+if [ $? -ne 0 ];then
+  echo "failed ./gradlew ${TASK}"
+  exit 1
+fi
+
 echo "Done"
 
